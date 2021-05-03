cfbcbf8343e7bda385fcf8b37ff07d163a7b1192
Tim Stumbaugh <me@tjstum.com>
2021-05-03 08:17:31 -0600

    fix typo in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ LONG_DESC = open("README.rst", encoding="utf-8").read()
 setup(
     name="tricycle",
     version=__version__,
-    description="Experiemntal extensions for Trio, the friendly async I/O library",
+    description="Experimental extensions for Trio, the friendly async I/O library",
     url="https://github.com/oremanj/tricycle",
     long_description=LONG_DESC,
     author="Joshua Oreman",
